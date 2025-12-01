0d28c9c0d72d31a485f384d0cbe76e1e85f046a2
michael-de-wit <michael.b.dewit@gmail.com>
2025-12-01 14:43:59 -0800

    initial pass employee crud

--- postgres_crm_lab.py
+++ postgres_crm_lab.py
@@ -71,14 +71,114 @@ while user_wants_to_quit == False:
             cursor.execute("SELECT * FROM companies")
             print(cursor.fetchall())
             id_of_company_to_remove = input("\n Which company do you want to remove? company id: ")
-            # try:
-            cursor.execute("SELECT company_name from companies WHERE id = %s", [id_of_company_to_remove])
-            name_of_company_to_remove = cursor.fetchone()[0]
-            cursor.execute("DELETE FROM companies WHERE id = %s", [id_of_company_to_remove])
-            connection.commit()
-            print(f"Removed {id_of_company_to_remove} {name_of_company_to_remove}")
-            # except:
-            #     print("Not removed. Something went wrong.")
+            try:
+                cursor.execute("SELECT company_name from companies WHERE id = %s", [id_of_company_to_remove])
+                name_of_company_to_remove = cursor.fetchone()[0]
+                cursor.execute("DELETE FROM companies WHERE id = %s", [id_of_company_to_remove])
+                connection.commit()
+                print(f"Removed {id_of_company_to_remove} {name_of_company_to_remove}")
+            except:
+                print("Not removed. Something went wrong.")
+    elif user_wants_to_modify_employees == True:
+        user_response_action_to_take_on_employees_table = input("\n What do you want to do with the employees table? " \
+        "\n 1. View employees" \
+        "\n 2. Add employee" \
+        "\n 3. Modify employee data" \
+        "\n 4. Remove employee" \
+        "\n 5. quit" \
+        "\n\n response: ")  
+
+        user_wants_to_view_employees = True if user_response_action_to_take_on_employees_table == '1' else False
+        user_wants_to_add_employee = True if user_response_action_to_take_on_employees_table == '2' else False
+        user_wants_to_modify_employee = True if user_response_action_to_take_on_employees_table == '3' else False
+        user_wants_to_remove_employee = True if user_response_action_to_take_on_employees_table == '4' else False
+        user_wants_to_quit = True if user_response_action_to_take_on_employees_table == '5' else False
+
+        if user_wants_to_view_employees == True:
+            try:
+                print("")
+                cursor.execute("SELECT * FROM employees")
+                print(cursor.fetchall())
+            except:
+                print("Something went wrong.")
+        elif user_wants_to_add_employee:
+            name_of_employee_to_add = input("\n Name of employee to add: ")
+            try:
+                cursor.execute("INSERT INTO employees (employee_name) VALUES(%s)", [name_of_employee_to_add])
+                connection.commit()
+                cursor.execute("SELECT id from employees WHERE employee_name LIKE %s", [name_of_employee_to_add])
+                id_of_added_employee = cursor.fetchone()[0]
+                print(f"\n Added id: {id_of_added_employee}; employee name: {name_of_employee_to_add}")
+            except psycopg2.errors.UniqueViolation:
+                connection.rollback()
+                cursor.execute("SELECT id from employees WHERE employee_name LIKE %s", [name_of_employee_to_add])
+                id_of_added_employee = cursor.fetchone()[0]
+                print(f"Not added. The employee {id_of_added_employee} {name_of_employee_to_add} already exists")
+            
+            cursor.execute("SELECT * FROM companies")
+            print(cursor.fetchall())
+            company_id_of_employee_to_add = input("\n Company id of employee: ")
+            print("")
+            try:
+                cursor.execute("UPDATE employees SET company_id=%s WHERE id = %s", [company_id_of_employee_to_add, id_of_added_employee])
+                connection.commit()
+
+                cursor.execute("SELECT company_name from companies WHERE id = %s", [company_id_of_employee_to_add])
+                name_of_company_of_employee_to_add = cursor.fetchone()[0]    
+                print(f"Add eid {id_of_added_employee} {name_of_employee_to_add} to cid {company_id_of_employee_to_add} {name_of_company_of_employee_to_add}")
+            except:
+                print("Company id not added. Something went wrong.")
+
+        elif user_wants_to_modify_employee:
+            cursor.execute("SELECT * FROM employees")
+            print(cursor.fetchall())
+            id_of_employee_to_modify = input("\n Which employee do you want to modify? employee id: ")
+            print("")
+            cursor.execute("SELECT employee_name from employees WHERE id = %s", [id_of_employee_to_modify])
+            name_of_employee_to_modify = cursor.fetchone()[0]
+
+            cursor.execute("SELECT company_id from employees WHERE id = %s", [id_of_employee_to_modify])
+            company_id_of_employee_to_modify = cursor.fetchone()[0]    
+
+            cursor.execute("SELECT company_name from companies WHERE id = %s", [company_id_of_employee_to_modify])
+            name_of_company_of_employee_to_modify = cursor.fetchone()[0]    
+
+            user_response_action_to_take_on_employee_modification = input(f"\n What do you want to modify for eid {id_of_employee_to_modify} {name_of_employee_to_modify} from cid {company_id_of_employee_to_modify} {name_of_company_of_employee_to_modify}? \n 1. Employee name \n 2. Company ID \n 3. quit ")
+
+            user_wants_to_modify_employee_name = True if user_response_action_to_take_on_employee_modification == '1' else False
+            user_wants_to_modify_employee_company_id = True if user_response_action_to_take_on_employee_modification == '2' else False
+            user_wants_to_quit = True if user_response_action_to_take_on_employee_modification == '3' else False    
+
+            if user_wants_to_modify_employee_name == True:
+                new_name_of_employee_to_modify = input(f"New name for {name_of_employee_to_modify}: ")
+                try:
+                    cursor.execute("UPDATE employees SET employee_name=%s WHERE id = %s", [new_name_of_employee_to_modify, id_of_employee_to_modify])
+                    connection.commit()
+                except:
+                    print("Not updated. Something went wrong.")
+            if user_wants_to_modify_employee_company_id == True:
+                print("")
+                cursor.execute("SELECT * FROM companies")
+                print(cursor.fetchall())
+                new_company_id_of_employee_to_modify = input(f"New company ID for {name_of_employee_to_modify}: ")
+                try:
+                    cursor.execute("UPDATE employees SET company_id=%s WHERE id = %s", [new_company_id_of_employee_to_modify, id_of_employee_to_modify])
+                    connection.commit()
+                except:
+                    print("Not updated. Something went wrong.")
+        elif user_wants_to_remove_employee:
+            cursor.execute("SELECT * FROM employees")
+            print(cursor.fetchall())
+            id_of_employee_to_remove = input("\n Which employee do you want to remove? employee id: ")  
+
+            try:
+                cursor.execute("SELECT employee_name from employees WHERE id = %s", [id_of_employee_to_remove])
+                name_of_employee_to_remove = cursor.fetchone()[0]
+                cursor.execute("DELETE FROM employees WHERE id = %s", [id_of_employee_to_remove])
+                connection.commit()
+                print(f"\n Removed eid {id_of_employee_to_remove} {name_of_employee_to_remove}")
+            except:
+                print("Not removed. Something went wrong.")
              
-print('closing connection')
+print('\n closing connection')
 connection.close
